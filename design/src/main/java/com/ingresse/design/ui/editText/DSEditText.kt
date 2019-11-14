8173e1cc0700cf7bd92690c805f636e5c7f9d8b7
Felipe Leme <lemefe21@gmail.com>
2019-11-14 16:37:30 -0300

    change to work with visibility of spinner list

--- design/src/main/java/com/ingresse/design/ui/editText/DSEditText.kt
+++ design/src/main/java/com/ingresse/design/ui/editText/DSEditText.kt
@@ -1,7 +1,6 @@
 package com.ingresse.design.ui.editText
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.Typeface
 import android.text.Editable
