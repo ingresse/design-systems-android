63140e8777233bc4894dd2e0ce9d84806413f91d
Tahbata Ali <tahbata@msn.com>
2019-12-10 18:13:58 -0300

    Change background color tint method

--- design/src/main/java/com/ingresse/design/ui/button/DSFacebookButton.kt
+++ design/src/main/java/com/ingresse/design/ui/button/DSFacebookButton.kt
@@ -2,16 +2,18 @@ package com.ingresse.design.ui.button
 
 import android.content.Context
 import android.content.res.ColorStateList
-import android.graphics.PorterDuff
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import com.ingresse.design.R
 import com.ingresse.design.helper.ResourcesHelper
 import kotlinx.android.synthetic.main.custom_facebook_button.view.*
 
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class DSFacebookButton(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
     private val resHelper = ResourcesHelper(context)
     private val mainLayout: ConstraintLayout get() = layout_facebook_button
@@ -34,8 +36,8 @@ class DSFacebookButton(context: Context, attrs: AttributeSet) : FrameLayout(cont
 
         val bgColors = ColorStateList(states, colors)
 
-        ViewCompat.setBackgroundTintList(mainLayout, bgColors)
-        ViewCompat.setBackgroundTintMode(mainLayout, PorterDuff.Mode.MULTIPLY)
-        ViewCompat.setBackground(mainLayout, resHelper.getDrawableHelper(R.drawable.btn_solid_bg))
+        val drawable = DrawableCompat.wrap(resHelper.getDrawableHelper(R.drawable.btn_solid_bg))
+        drawable.setTintList(bgColors)
+        mainLayout.background = drawable
     }
 }
