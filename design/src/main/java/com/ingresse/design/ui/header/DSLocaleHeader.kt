48ab7331b1108a9df09860578f35d23dde0bf739
Felipe Leme <lemefe21@gmail.com>
2020-04-15 16:34:56 -0300

    add back button in locale header

--- design/src/main/java/com/ingresse/design/ui/header/DSLocaleHeader.kt
+++ design/src/main/java/com/ingresse/design/ui/header/DSLocaleHeader.kt
@@ -2,13 +2,23 @@ package com.ingresse.design.ui.header
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
 import com.ingresse.design.R
 import com.ingresse.design.helper.setVisible
+import kotlinx.android.synthetic.main.custom_header.view.*
 import kotlinx.android.synthetic.main.ds_locale_header.view.*
+import kotlinx.android.synthetic.main.ds_locale_header.view.btn_back
 
 class DSLocaleHeader(context: Context, attrs: AttributeSet): FrameLayout(context, attrs) {
-    init { inflate(context, R.layout.ds_locale_header, this) }
+    init {
+        inflate(context, R.layout.ds_locale_header, this)
+
+        val array = context.theme.obtainStyledAttributes(attrs, R.styleable.DSLocaleHeader, 0, 0)
+        val returnType = array.getBoolean(R.styleable.DSLocaleHeader_returnType, false)
+
+        if(returnType) btn_back.visibility = View.VISIBLE
+    }
 
     fun setHeader(category: String? = null, place: String, onPlaceClick: () -> Unit) {
         txt_place.text = place
@@ -18,4 +28,9 @@ class DSLocaleHeader(context: Context, attrs: AttributeSet): FrameLayout(context
     }
 
     fun updatePlace(place: String) { txt_place.text = place }
+
+    fun setCloseAction(action: () -> Unit) {
+        val listener = OnClickListener { action() }
+        btn_back.setOnClickListener(listener)
+    }
 }
