6a9da6b4a913a0324fcb45b1de8823b1d6e7216f
PhillipiLino <phillipi.lino@hotmail.com>
2020-06-09 20:26:09 -0300

    Add fragment extension to hide keyboard

--- design/src/main/java/com/ingresse/design/helper/Fragment.kt
+++ design/src/main/java/com/ingresse/design/helper/Fragment.kt
@@ -1,5 +1,8 @@
 package com.ingresse.design.helper
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 
@@ -11,4 +14,11 @@ fun Fragment.sendFragmentToFront() {
 fun Fragment.sendFragmentToBack() {
     val fragmentView = view ?: return
     ViewCompat.setTranslationZ(fragmentView, 0f)
+}
+
+fun Fragment.hideKeyboard() {
+    val ctx = context ?: return
+    val currentView = view ?: View(ctx)
+    val inputManager = ctx.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    inputManager?.hideSoftInputFromWindow(currentView.windowToken, 0)
 }
