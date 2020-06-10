bce6e8c9f7283515d103bbd19b6f0d8b7e543601
PhillipiLino <phillipi.lino@hotmail.com>
2020-06-10 12:41:46 -0300

    [ING-617] Create DSListDialog component

---
+++ design/src/main/java/com/ingresse/design/ui/dialog/DSListDialog.kt
@@ -0,0 +1,27 @@
+package com.ingresse.design.ui.dialog
+
+import android.content.Context
+import android.util.AttributeSet
+import com.ingresse.design.R
+import com.ingresse.design.ui.adapters.BaseAdapter
+import kotlinx.android.synthetic.main.ds_list_dialog.*
+
+class DSListDialog(context: Context): DSBaseDialog(R.layout.ds_list_dialog, context) {
+    /**
+     * Function to set dialog properties
+     *
+     * @param title - Title to show on top of dialog.
+     * @param adapter - Adapter to control list inside dialog.
+     */
+    fun <T>setDialog(title: String, adapter: BaseAdapter<T>) {
+        lbl_dialog_title.text = title
+        recycler.adapter = adapter
+    }
+
+    /**
+     * Function to set dialog recycler minimum height
+     *
+     * @param height - Height to set to minimum.
+     */
+    fun setRecyclerMinHeight(height: Int) { recycler.minimumHeight = height }
+}
